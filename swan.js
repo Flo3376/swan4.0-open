756bca918216e3d3fe7e6271679b1c2e57060eb5
Flo3376 <Flo3376@users.noreply.github.com>
2024-09-07 18:09:42 +0200

    maj

--- swan.js
+++ swan.js
@@ -334,7 +334,7 @@ const google_set = google_data.every(google_data => google_test[google_data] && 
 
 //vérification qu'un vocaliser est utilisable
 if ((config.vocalisation.engine == "revoicer" && revoicer_set) || (config.vocalisation.engine == "openAI" && openAI_set) || (config.vocalisation.engine == "google" && google_set)) {
-    console.log(colors.green(`Moteur de vocalisation sélectionné : `) + config.vocalisation);
+    console.log(colors.green(`Moteur de vocalisation sélectionné : `) + config.vocalisation.engine);
 } else {
     if(config.vocalisation!="sound_bank"){
         console.log(colors.red(`Le moteur de vocalisation "${config.vocalisation}" ne peut être utilisé, veuillez vérifier votre fichier de config, coupure du programme`));
